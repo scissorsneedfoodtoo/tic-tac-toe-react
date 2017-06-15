6eb5fc1c2ce49f3ff5c678231c1e054934c4eb03
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-06-15 22:52:22 +0900

    added material icons cdn

--- src/App.js
+++ src/App.js
@@ -2,24 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
-
 class App extends React.Component {
 
   constructor(props) {
@@ -642,5 +624,3 @@ class App extends React.Component {
 } // end App Component
 
 export default App;
-
-// ReactDOM.render(<App />, document.getElementById("app"));
